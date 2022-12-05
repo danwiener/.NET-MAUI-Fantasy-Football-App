818e62f441833f5bef61a7d46bd1adf92fecf836
Henrik Wiener <henrik.wiener@outlook.com>
2022-12-05 11:37:43 -0800

    Updated OpeningPage UI

--- Views/OpeningPage.xaml.cs
+++ Views/OpeningPage.xaml.cs
@@ -10,16 +10,16 @@ public partial class OpeningPage : ContentPage
 	protected async override void OnAppearing()
 	{
 		base.OnAppearing();
-		Task scaleTitle = Task.Factory.StartNew(async () => { await TitleLabel.ScaleTo(1, 3000); });
+		Task scaleTitle = Task.Factory.StartNew(async () => { await TitleLabel.ScaleTo(1, 2000); });
 
 		await Task.Delay(1000);
-		Task rotateLabel1 = Task.Factory.StartNew(async () => { await SignInLabel.RotateYTo(35, 2000); });
-		Task rotateLabel2 = Task.Factory.StartNew(async () => { await SignUpLabel.RotateYTo(-35, 2000); });
-		Task rotateLeftGrid = Task.Factory.StartNew(async () => { await LeftGrid.RotateYTo(35, 2000); });
-		Task rotateRightGrid = Task.Factory.StartNew(async () => { await RightGrid.RotateYTo(-35, 2000); });
-		Task rotateFootball = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(-75, 700); });
-		await Task.Delay(700);
-		Task rotateFootball2 = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(85, 800); });
+		Task rotateLabel1 = Task.Factory.StartNew(async () => { await SignInLabel.RotateYTo(35, 1200); });
+		Task rotateLabel2 = Task.Factory.StartNew(async () => { await SignUpLabel.RotateYTo(-35, 1200); });
+		Task rotateLeftGrid = Task.Factory.StartNew(async () => { await LeftGrid.RotateYTo(35, 1200); });
+		Task rotateRightGrid = Task.Factory.StartNew(async () => { await RightGrid.RotateYTo(-35, 1200); });
+		Task rotateFootball = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(85, 300); });
+		//await Task.Delay(700);
+		//Task rotateFootball2 = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(85, 800); });
 	} // End method
 
 	//private void OnLoginBtnClicked(object sender, EventArgs e)
