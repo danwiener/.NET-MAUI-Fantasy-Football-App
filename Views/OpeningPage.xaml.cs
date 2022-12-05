70091ef9458d38490daea816d69b8b06c02e1bb7
Henrik Wiener <henrik.wiener@outlook.com>
2022-12-05 09:31:13 -0800

    Updated UI

--- Views/OpeningPage.xaml.cs
+++ Views/OpeningPage.xaml.cs
@@ -11,11 +11,15 @@ public partial class OpeningPage : ContentPage
 	{
 		base.OnAppearing();
 		Task scaleTitle = Task.Factory.StartNew(async () => { await TitleLabel.ScaleTo(1, 3000); });
+
+		await Task.Delay(1000);
 		Task rotateLabel1 = Task.Factory.StartNew(async () => { await SignInLabel.RotateYTo(35, 2000); });
 		Task rotateLabel2 = Task.Factory.StartNew(async () => { await SignUpLabel.RotateYTo(-35, 2000); });
 		Task rotateLeftGrid = Task.Factory.StartNew(async () => { await LeftGrid.RotateYTo(35, 2000); });
 		Task rotateRightGrid = Task.Factory.StartNew(async () => { await RightGrid.RotateYTo(-35, 2000); });
-		Task rotateFootball = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(85, 2500); });
+		Task rotateFootball = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(-75, 700); });
+		await Task.Delay(700);
+		Task rotateFootball2 = Task.Factory.StartNew(async () => { await LoginGrid.RotateXTo(85, 800); });
 	} // End method
 
 	//private void OnLoginBtnClicked(object sender, EventArgs e)
